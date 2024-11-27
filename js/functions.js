d5d43786be7cbdbf448e9c52425c7cc9423caf40
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-27 11:32:06 +0100

    feat: added debounce( ) function - js/functions.js

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+/**
+ * Description: function calls the given function [functionToCall] only after time param [delay].
+ * @param {function} functionToCall
+ * @param {number} delay
+ * @returns {function} 
+ */
+function debounce(functionToCall, delay) {
+
+   // Define variable for timeout
+   let timeout;
+
+   return function (...args) { // capture arguments passed to the debounced function
+
+      // Clear the previous timer
+      clearTimeout(timeout);
+
+      // Capture context of function
+      const context = this;
+
+      // Set a new timeout to call the function
+      timeout = setTimeout(() => {
+
+         // Execute the function with the correct context and arguments
+         functionToCall.apply(context, args);
+
+      }, delay);
+
+   };
+
+};
