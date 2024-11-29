9031f7336cc9797bcdca0aa330c995972bcf5635
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-29 16:51:02 +0100

    feat: add detailed comments - js/functions.js

--- js/functions.js
+++ js/functions.js
@@ -137,7 +137,7 @@ function chooseWeapons(fighters, weapons) {
       fightersWithWeapons.push(fighterWithWeapon);
 
       // Log the details of chosen weapon and calculated total power to the console.
-      console.log(`%c${fighterWithWeapon.name} %c(power: %c${fighterWithWeapon.power}%c) chose %c${fighterWithWeapon.weapon.name} %c(power: %c${fighterWithWeapon.weapon.power}%c) increasing its power to: %c${fighterWithWeapon.totalPower}`, styleFighters, styleDefault, stylePowers, styleDefault, styleWeapons, styleDefault, stylePowers, styleDefault,stylePowers);
+      console.log(`%c${fighterWithWeapon.name} %c(power: %c${fighterWithWeapon.power}%c) chose %c${fighterWithWeapon.weapon.name} %c(power: %c${fighterWithWeapon.weapon.power}%c) increasing its power to: %c${fighterWithWeapon.totalPower}`, styleFighters, styleDefault, stylePowers, styleDefault, styleWeapons, styleDefault, stylePowers, styleDefault, stylePowers);
 
    });
 
@@ -230,58 +230,68 @@ function qualification(trainedFighters) {
 };
 
 /**
- * Description: function for conducting the combat phase between fighters, returning winners of each match.
- * @param {array} qualifiedFighters
- * @returns {array}
+ * Description: function conducts combat between pairs of fighters, determining the winners based on their trained power.
+ * @param {array} qualifiedFighters - An array of fighters who have qualified for combat.
+ * @returns {array} - An array of winners after the combat.
  */
 function combat(qualifiedFighters) {
 
+   // Array to keep track of all the winners
    let winners = [];
 
+   // Set to track losers
    let losers = new Set();
 
+   // Count rounds of combat
    let roundsCount = 1;
 
+   // Continue combat until fewer than two fighters remain.
    while (qualifiedFighters.length > 1) {
 
-      // Ensure that the number of qualified fighters is even
+      // Check if there is an odd number of qualified fighters.
       if (qualifiedFighters.length % 2 !== 0) {
 
-         // Add a robot fighter if the number is odd
+         // If odd, add a robot fighter to balance the numbers.
          qualifiedFighters.push({ name: "Robot", trainedPower: 4000 });
 
          console.log('%cAdded Robot fighter for balance.', styleFighters);
 
       };
 
+      // Array to store winners for the current round.
       let roundWinners = [];
 
+      // Count combats
       let combatsCounter = 1;
 
-      console.log(`%c${qualifiedFighters.length < 5 ? (qualifiedFighters.length < 3 ? 'Final' : 'Semifinal') : `${roundsCount}° Turn` }`, styleTurns);
+      // Log the round count and type (Final, Semifinal, etc.)
+      console.log(`%c${qualifiedFighters.length < 5 ? (qualifiedFighters.length < 3 ? 'Final' : 'Semifinal') : `${roundsCount}° Turn`}`, styleTurns);
 
-      // Loop through the fighters in pairs
+      // Loop through the fighters in pairs to conduct combat.
       for (let i = 0; i < qualifiedFighters.length; i += 2) {
 
          const fighterOne = qualifiedFighters[i];
 
          const fighterTwo = qualifiedFighters[i + 1];
 
-
+         // Log the current combat matchup.
          console.log(`%cCombat n°${combatsCounter}:%c ${fighterOne.name} %cvs %c${fighterTwo.name}`, styleCombats, styleFighters, styleDefault, styleFighters);
 
          combatsCounter++;
 
-         // Determine the winner based on total power
+         // Determine the winner based on trained power
          const winner = fighterOne.trainedPower > fighterTwo.trainedPower ? fighterOne : fighterTwo;
 
-         // Determine the loser based on total power
+         // Determine the loser based on trained power
          const loser = fighterOne.trainedPower < fighterTwo.trainedPower ? fighterOne : fighterTwo;
 
+         // Store the winner for this round.
          roundWinners.push(winner);
 
+         // Track the loser to prevent them from participating again.
          losers.add(loser);
 
+         // Log the winner's name and their final power.
          console.log(`%c- Winner: %c${winner.name}%c with a final power of: %c${winner.trainedPower}`, styleDefault, styleFighters, styleDefault, stylePowers);
 
       };
@@ -290,35 +300,43 @@ function combat(qualifiedFighters) {
 
       // console.log("losers", typeof losers, losers);
 
-      // Update qualifiedFighters for the next round
+      // Update qualifiedFighters for the next round.
       qualifiedFighters = roundWinners;
 
       roundsCount++;
 
    };
 
+   // Check if only one fighter remains as the overall winner.
    if (qualifiedFighters.length == 1) {
 
+      // Add the overall winner to the winners array.
       winners.push(qualifiedFighters[0]);
 
       // console.log("winners", typeof winners, winners);
 
+      // Convert Set to Array for processing.
       losers = [...losers];
 
       // console.log("losers", typeof losers, losers);
 
+      // Add the last loser for 2nd place consideration.
       winners.push(losers[losers.length - 1]);
 
       // console.log("winners", typeof winners, winners);
 
+      // Log the current combat matchup.
       console.log(`%cRound for third position`, styleTurns);
 
       console.log(`%cCombat: %c${losers[losers.length - 2].name}%c vs %c${losers[losers.length - 3].name}`, styleCombats, styleFighters, styleDefault, styleFighters);
 
+      // Determine the winner of the 3rd place match.
       const playoffWinner = losers[losers.length - 2].trainedPower > losers[losers.length - 3].trainedPower ? losers[losers.length - 2] : losers[losers.length - 3];
 
+      // Log the playoff winner's name and their final power.
       console.log(`%c- Winner: %c${playoffWinner.name}%c with a final power of: %c${playoffWinner.trainedPower}`, styleDefault, styleFighters, styleDefault, stylePowers);
 
+      // Add 3rd place winner to the winners array.
       winners.push(playoffWinner);
 
       // console.log("winners", typeof winners, winners);
@@ -332,9 +350,8 @@ function combat(qualifiedFighters) {
 };
 
 /**
- * Description: function for determining the top three fighters based on trained power.
- * @param {array} combatWinners
- * @returns {array}
+ * Description: function finalizes the results by logging the top three fighters based on their performance in the combat phase.
+ * @param {array} combatWinners - Array of fighters who won their respective matches.
  */
 function awardCeremony(combatWinners) {
 
@@ -343,8 +360,10 @@ function awardCeremony(combatWinners) {
    // Log the winners for the podium
    console.log(`%cPodium Winners:`, styleTurns);
 
+   // Loop through the array of combat winners and log their standings.
    for (let index = combatWinners.length - 1; index >= 0; index--) {
 
+      // Log the winners of each podium position
       console.log(`%c${index + 1}° place: %c${combatWinners[index].name}%c with final power of %c${combatWinners[index].trainedPower}`, styleDefault, styleFighters, styleDefault, stylePowers);
 
    };
